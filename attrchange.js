e7838fb8793c3d2a9cb29710760ca6412309ed8e
Selvakumar Arumugam <meetselva@gmail.com>
2012-10-11 18:49:41 -0300

    Update attrchange.js

--- attrchange.js
+++ attrchange.js
@@ -1,5 +1,20 @@
 /*
 A simple jQuery function that can be used to add listeners on attribute change.
+
+Copyright (C) (2011-2012) Selvakumar Arumugam
+
+This program is free software: you  can redistribute it and/or modify it
+under the  terms of the GNU  General Public License as  published by the
+Free Software Foundation,  either version 3 of the License,  or (at your
+option) any later version.
+
+This  program  is distributed  in  the  hope  that  it will  be  useful,
+but  WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy  of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 //Initial version of a cross-browser proof attrchange listener. (How proof is it? yet to be tested :) )
 (function($) {
