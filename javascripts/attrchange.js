c394ee76f75679c06ce6fbf1c8770a5fda0d7ba8
meetselva <meetselva@gmail.com>
2013-05-03 15:18:39 -0400

    Updated from master

--- javascripts/attrchange.js
+++ javascripts/attrchange.js
@@ -1,72 +1,112 @@
 /*
-A simple jQuery function that can be used to add listeners on attribute change.
+A simple jQuery function that can add listeners on attribute change.
+http://meetselva.github.io/attrchange/
 
-Copyright (C) (2011-2012) Selvakumar Arumugam
-
-This program is free software: you  can redistribute it and/or modify it
-under the  terms of the GNU  General Public License as  published by the
-Free Software Foundation,  either version 3 of the License,  or (at your
-option) any later version.
-
-This  program  is distributed  in  the  hope  that  it will  be  useful,
-but  WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-Public License for more details.
-
-You should have received a copy  of the GNU General Public License along
-with this program. If not, see <http://www.gnu.org/licenses/>.
+About License:
+Copyright (C) 2013 Selvakumar Arumugam
+You may use attrchange plugin under the terms of the MIT Licese.
+https://github.com/meetselva/attrchange/blob/master/MIT-License.txt
 */
 (function($) {
    function isDOMAttrModifiedSupported() {
-	var p = document.createElement('p');
-	var flag = false;
-
-	if (p.addEventListener) p.addEventListener('DOMAttrModified', function() {
-		flag = true
-	}, false);
-	else if (p.attachEvent) p.attachEvent('onDOMAttrModified', function() {
-		flag = true
-	});
-	else return false;
-
-	p.setAttribute('id', 'target');
-
-	return flag;
+		var p = document.createElement('p');
+		var flag = false;
+		
+		if (p.addEventListener) p.addEventListener('DOMAttrModified', function() {
+			flag = true
+		}, false);
+		else if (p.attachEvent) p.attachEvent('onDOMAttrModified', function() {
+			flag = true
+		});
+		else return false;
+		
+		p.setAttribute('id', 'target');
+		
+		return flag;
+   }
+   
+   function checkAttributes(chkAttr, e) {
+		if (chkAttr) {
+			var attributes = this.data('attr-old-value');
+			
+			if (e.attributeName.indexOf('style') >= 0) {
+				if (!attributes['style']) attributes['style'] = {}; //initialize
+				var keys = e.attributeName.split('.'); 				
+				e.oldValue = attributes['style'][keys[1]];
+				attributes['style'][keys[1]] = this.prop("style")[$.camelCase(keys[1])];
+			} else {
+				e.oldValue = attributes[e.attributeName];
+				attributes[e.attributeName] = this.attr(e.attributeName); 
+			}
+			
+			this.data('attr-old-value', attributes); //update the old value object
+		}	   
    }
 
    //initialize Mutation Observer
-   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-   $.fn.attrchange = function(callback) {	  
+   $.fn.attrchange = function(o) {
+	   
+		var cfg = {
+			attributeOldValue: false,
+			callback: $.noop
+		};
+		
+		//for backward compatibility
+		if (typeof o === "function" ) { 
+			cfg.callback = o; 
+		} else { 
+			$.extend(cfg, o); 
+		}
+
+	    if (cfg.attributeOldValue) { //get attributes old value
+	    	$(this).each(function (i, el) {
+	    		var attributes = {};
+	    		for (var attr, i=0, attrs=el.attributes, l=attrs.length; i<l; i++){
+	    		    attr = attrs.item(i);
+	    		    attributes[attr.nodeName] = attr.value;
+	    		}
+	    		
+	    		$(this).data('attr-old-value', attributes);
+	    	});
+	    }
+	   
 		if (MutationObserver) { //Modern Browsers supporting MutationObserver
 			/*
 			   Mutation Observer is still new and not supported by all browsers. 
 			   http://lists.w3.org/Archives/Public/public-webapps/2011JulSep/1622.html
 			*/
-			var options = {
+			var mOptions = {
 				subtree: false,
 				attributes: true,
-				attributeOldValue: true
+				attributeOldValue: cfg.attributeOldValue
 			};
 	
 			var observer = new MutationObserver(function(mutations) {
 				mutations.forEach(function(e) {
-					callback.call(e.target, e, e.attributeName);
+					cfg.callback.call(e.target, e);
 				});
 			});
 	
 			return this.each(function() {
-				observer.observe(this, options);
+				observer.observe(this, mOptions);
 			});
 		} else if (isDOMAttrModifiedSupported()) { //Opera
 			//Good old Mutation Events but the performance is no good
 			//http://hacks.mozilla.org/2012/05/dom-mutationobserver-reacting-to-dom-changes-without-killing-browser-performance/
 			return this.on('DOMAttrModified', function(e) {
-				callback.call(this, e, e.attrName);
+				e.attributeName = e.attrName;
+				//to set the attr old value
+				checkAttributes.call($(this), cfg.attributeOldValue , e);
+				cfg.callback.call(this, e);
 			});
 		} else if ('onpropertychange' in document.body) { //works only in IE		
 			return this.on('propertychange', function(e) {
-				callback.call(this, e, window.event.propertyName);
+				e.attributeName = window.event.propertyName;
+				//to set the attr old value
+				checkAttributes.call($(this), cfg.attributeOldValue , e);
+				cfg.callback.call(this, e);
 			});
 		}
 
