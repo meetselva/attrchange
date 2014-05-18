6e2cd8df1c5e365b6fd0400ae270a1a6955f4785
meetselva <meetselva@gmail.com>
2014-05-17 21:56:04 -0400

    1. Updated plugin code to support extensions 2. Added disconnect feature to remove attrchange listener

--- attrchange.js
+++ attrchange.js
@@ -18,10 +18,8 @@ https://github.com/meetselva/attrchange/blob/master/MIT-License.txt
 		else if (p.attachEvent) p.attachEvent('onDOMAttrModified', function() {
 			flag = true
 		});
-		else return false;
-		
-		p.setAttribute('id', 'target');
-		
+		else return false;		
+		p.setAttribute('id', 'target');		
 		return flag;
    }
    
@@ -49,81 +47,93 @@ https://github.com/meetselva/attrchange/blob/master/MIT-License.txt
    //initialize Mutation Observer
    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-   $.fn.attrchange = function(o) {
-	   
-		var cfg = {
-			trackValues: false,
-			callback: $.noop
-		};
-		
-		//for backward compatibility
-		if (typeof o === "function" ) { 
-			cfg.callback = o; 
-		} else { 
-			$.extend(cfg, o); 
-		}
+   $.fn.attrchange = function(a, b) {
+	   if (typeof a == 'object') {//core
+		   return attrchangeFx._core.call(this, a);
+	   } else if (typeof a == 'string') { //extensions/options
+		   return attrchangeFx._ext.call(this, a, b);
+	   }
+   }
+   
+   var attrchangeFx = {
+		   _core: function (o) {
+			   var cfg = {
+					trackValues: false,
+					callback: $.noop
+				};
 
-	    if (cfg.trackValues) { //get attributes old value
-	    	$(this).each(function (i, el) {
-	    		var attributes = {};
-	    		for (var attr, i=0, attrs=el.attributes, l=attrs.length; i<l; i++){
-	    		    attr = attrs.item(i);
-	    		    attributes[attr.nodeName] = attr.value;
-	    		}
-	    		
-	    		$(this).data('attr-old-value', attributes);
-	    	});
-	    }
-	   
-		if (MutationObserver) { //Modern Browsers supporting MutationObserver
-			/*
-			   Mutation Observer is still new and not supported by all browsers. 
-			   http://lists.w3.org/Archives/Public/public-webapps/2011JulSep/1622.html
-			*/
-			var mOptions = {
-				subtree: false,
-				attributes: true,
-				attributeOldValue: cfg.trackValues
-			};
-	
-			var observer = new MutationObserver(function(mutations) {
-				mutations.forEach(function(e) {
-					var _this = e.target;
-					
-					//get new value if trackValues is true
-					if (cfg.trackValues) {
-						/**
-						 * @KNOWN_ISSUE: The new value is buggy for STYLE attribute as we don't have 
-						 * any additional information on which style is getting updated. 
-						 * */
-						e.newValue = $(_this).attr(e.attributeName);
-					}
-					
-					cfg.callback.call(_this, e);
-				});
-			});
-	
-			return this.each(function() {
-				observer.observe(this, mOptions);
-			});
-		} else if (isDOMAttrModifiedSupported()) { //Opera
-			//Good old Mutation Events but the performance is no good
-			//http://hacks.mozilla.org/2012/05/dom-mutationobserver-reacting-to-dom-changes-without-killing-browser-performance/
-			return this.on('DOMAttrModified', function(event) {
-				if (event.originalEvent) event = event.originalEvent; //jQuery normalization is not required for us 
-				event.attributeName = event.attrName; //property names to be consistent with MutationObserver
-				event.oldValue = event.prevValue; //property names to be consistent with MutationObserver 
-				cfg.callback.call(this, event);
-			});
-		} else if ('onpropertychange' in document.body) { //works only in IE		
-			return this.on('propertychange', function(e) {
-				e.attributeName = window.event.propertyName;
-				//to set the attr old value
-				checkAttributes.call($(this), cfg.trackValues , e);
-				cfg.callback.call(this, e);
-			});
-		}
+				//backward compatibility
+				if (typeof o === "function" ) {	cfg.callback = o; } else { 	$.extend(cfg, o); }
 
-		return this;
-    }
+			    if (cfg.trackValues) { //get attributes old value
+			    	this.each(function (i, el) {
+			    		var attributes = {};
+			    		for (var attr, i=0, attrs=el.attributes, l=attrs.length; i<l; i++){
+			    		    attr = attrs.item(i);
+			    		    attributes[attr.nodeName] = attr.value;
+			    		}
+			    		$(this).data('attr-old-value', attributes);
+			    	});
+			    }
+			   
+				if (MutationObserver) { //Modern Browsers supporting MutationObserver
+					var mOptions = {
+						subtree: false,
+						attributes: true,
+						attributeOldValue: cfg.trackValues
+					};
+					var observer = new MutationObserver(function(mutations) {
+						mutations.forEach(function(e) {
+							var _this = e.target;
+							//get new value if trackValues is true
+							if (cfg.trackValues) {
+								/**
+								 * @KNOWN_ISSUE: The new value is buggy for STYLE attribute as we don't have 
+								 * any additional information on which style is getting updated. 
+								 * */
+								e.newValue = $(_this).attr(e.attributeName);
+							}								
+							cfg.callback.call(_this, e);
+						});
+					});
+			
+					return this.data('attrchange-method', 'Mutation Observer')
+							.data('attrchange-obs', observer)
+							.each(function() {
+						observer.observe(this, mOptions);
+					});
+				} else if (isDOMAttrModifiedSupported()) { //Opera
+					//Good old Mutation Events
+					return this.data('attrchange-method', 'DOMAttrModified').on('DOMAttrModified', function(event) {
+						if (event.originalEvent) event = event.originalEvent; //jQuery normalization is not required 
+						event.attributeName = event.attrName; //property names to be consistent with MutationObserver
+						event.oldValue = event.prevValue; //property names to be consistent with MutationObserver 
+						cfg.callback.call(this, event);
+					});
+				} else if ('onpropertychange' in document.body) { //works only in IE		
+					return this.data('attrchange-method', 'propertychange').on('propertychange', function(e) {
+						e.attributeName = window.event.propertyName;
+						//to set the attr old value
+						checkAttributes.call($(this), cfg.trackValues , e);
+						cfg.callback.call(this, e);
+					});
+				}
+
+				return this;
+		   },
+		   _ext: function (s, o) { /*attrchange option/extension*/
+			   switch (s) {
+			   case 'disconnect':
+				   return this.each (function () {
+					   var attrchangeMethod = $(this).data('attrchange-method');
+					   if (attrchangeMethod == 'propertychange' || attrchangeMethod == 'DOMAttrModified') {
+						   $(this).off(attrchangeMethod);
+					   } else if (attrchangeMethod == 'Mutation Observer'){
+						   $(this).data('attrchange-obs').disconnect();
+					   }
+				   }).removeData('attrchange-method');
+			   }
+		   }
+   }
+
 })(jQuery);
